4139ad1496b8a9980c4869d01548c43193cb71fb
Eduardo <eduardoscf1210@gmail.com>
2025-01-07 11:22:31 -0300

    Correcao de bugs nos avisos quando o cliente tenta inicializar errado

--- client.c
+++ client.c
@@ -41,22 +41,53 @@ void *send_messages(void *arg) {
 
 // Função para validar os argumentos passados
 void validate_args(int argc, char **argv) {
+    // 1. Verificar número de argumentos
     if (argc < 7) {
         fprintf(stderr, "Error: Invalid number of arguments\n");
-        fprintf(stderr, "Usage: ./client <server_ip> <port> -type <type> -coords <x> <y>\n");
+        fprintf(stderr, "Usage: ./client <server_ip> <port> -type <temperature|humidity|air_quality> -coords <x> <y>\n");
         exit(EXIT_FAILURE);
     }
 
+    // 2. Verificar argumento "-type"
     if (strcmp(argv[3], "-type") != 0) {
         fprintf(stderr, "Error: Expected '-type' argument\n");
+        fprintf(stderr, "Usage: ./client <server_ip> <port> -type <temperature|humidity|air_quality> -coords <x> <y>\n");
         exit(EXIT_FAILURE);
     }
 
+    // 3. Verificar tipo de sensor
+    if (strcmp(argv[4], "temperature") != 0 &&
+        strcmp(argv[4], "humidity") != 0 &&
+        strcmp(argv[4], "air_quality") != 0) {
+        fprintf(stderr, "Error: Invalid sensor type\n");
+        fprintf(stderr, "Usage: ./client <server_ip> <port> -type <temperature|humidity|air_quality> -coords <x> <y>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // 4. Verificar argumento "-coords"
     if (strcmp(argv[5], "-coords") != 0) {
         fprintf(stderr, "Error: Expected '-coords' argument\n");
+        fprintf(stderr, "Usage: ./client <server_ip> <port> -type <temperature|humidity|air_quality> -coords <x> <y>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // 5. Verificar se as coordenadas são válidas
+    if (argc != 8) {
+        fprintf(stderr, "Error: Invalid number of arguments\n");
+        fprintf(stderr, "Usage: ./client <server_ip> <port> -type <temperature|humidity|air_quality> -coords <x> <y>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int x = atoi(argv[6]);
+    int y = atoi(argv[7]);
+
+    if (x < 0 || x > 9 || y < 0 || y > 9) {
+        fprintf(stderr, "Error: Coordinates must be in the range 0-9\n");
+        fprintf(stderr, "Usage: ./client <server_ip> <port> -type <temperature|humidity|air_quality> -coords <x> <y>\n");
         exit(EXIT_FAILURE);
     }
 }
+
 
 int main(int argc, char **argv) {
     validate_args(argc, argv);
@@ -100,6 +131,7 @@ int main(int argc, char **argv) {
         interval = 10;
     } else {
         fprintf(stderr, "Error: Invalid sensor type\n");
+        fprintf(stderr, "Usage: ./client <server_ip> <port> -type <temperature|humidity|air_quality> -coords <x> <y>\n");
         exit(EXIT_FAILURE);
     }
 
